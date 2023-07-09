5830720e59ac9a5509d543a2f99e30c28ad40a63
Roman Portianko <rportyanko10@gmail.com>
2023-07-09 10:14:07 +0300

    Modified CafeDbContext.cs to represent actual DB structure

--- CafeSystem/Data/CafeDbContext.cs
+++ CafeSystem/Data/CafeDbContext.cs
@@ -13,20 +13,8 @@ public class CafeDbContext : DbContext
     public DbSet<Category> Categories { get; set; }
 
     public DbSet<Dish> Dishes { get; set; }
+    
+    public DbSet<Order> Orders { get; set; }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-        
-        // seeding categories as for Italian cafe (user is able to delete this any time they wish)
-        modelBuilder.Entity<Category>().HasData(
-            new Category { Id = 1, Name = "Паста" },
-            new Category { Id = 2, Name = "Піца" },
-            new Category { Id = 3, Name = "Десерти" },
-            new Category { Id = 4, Name = "Кава" },
-            new Category { Id = 5, Name = "Чай" },
-            new Category { Id = 6, Name = "Безалкогольні напої" },
-            new Category { Id = 7, Name = "Алкогольні напої" }
-        );
-    }
+    public DbSet<OrderItem> OrderItems { get; set; }
 }
