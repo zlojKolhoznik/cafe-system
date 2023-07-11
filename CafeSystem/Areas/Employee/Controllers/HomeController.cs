dfdf9ed8543245bad929af50881c57b31068000f
Roman Portianko <rportyanko10@gmail.com>
2023-07-11 20:36:30 +0300

    Finished Identity. Admin now registers and deletes all other users and changes their roles

---
+++ CafeSystem/Areas/Employee/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CafeSystem.Areas.Employee.Controllers;
+
+[Area("Employee")]
+[Authorize(Roles = $"{StaticDetails.RoleCook}, {StaticDetails.RoleWaiter}")]
+public class HomeController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
